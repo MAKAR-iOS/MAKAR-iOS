7b58b04c184f07e6463a981a1fe420b4e7f0f1f4
Bibi-urssu <bibi.urssu@gmail.com>
2023-12-20 21:19:30 +0900

    Initial Commit

---
+++ MAKAR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAKAR
+//
+//  Created by 박지윤 on 2023/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
