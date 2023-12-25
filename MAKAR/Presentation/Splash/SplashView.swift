4bfe386d767bd6ac61af6bace2f1a911b76dc29a
Bibi-urssu <bibi.urssu@gmail.com>
2023-12-25 20:07:58 +0900

    feat: SplashView, SplashViewController 구현

--- MAKAR/Presentation/Splash/SplashView.swift
+++ MAKAR/Presentation/Splash/SplashView.swift
@@ -5,4 +5,43 @@
 //  Created by 박지윤 on 2023/12/25.
 //
 
-import Foundation
+import UIKit
+
+class SplashView: BaseView {
+
+    // MARK: UI Components
+    private let splashLabel = UILabel().then {
+        $0.text = "지하철 막차 알림 서비스"
+        $0.textColor = .black
+        $0.font = UIFont.systemFont(ofSize: 24)
+    }
+
+    private let splashImageView = UIImageView().then {
+        $0.image = MakarImage.makarLogo
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+
+        addSubview(splashLabel)
+        addSubview(splashImageView)
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+
+        splashLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(280)
+            $0.leading.equalToSuperview().inset(40)
+        }
+
+        splashImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(325)
+            $0.leading.equalToSuperview().inset(40)
+            $0.width.equalTo(200)
+            $0.height.equalTo(40)
+        }
+    }
+}
--- MAKAR/Presentation/Splash/SplashViewController.swift
+++ MAKAR/Presentation/Splash/SplashViewController.swift
@@ -5,4 +5,40 @@
 //  Created by 박지윤 on 2023/12/25.
 //
 
-import Foundation
+import UIKit
+
+class SplashViewController: BaseViewController {
+    
+    // MARK: UI Components
+    private let splashView = SplashView()
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1.8) {
+            let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+            sceneDelegate?.changeRootViewToLoginViewController()
+        }
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+
+        view.addSubview(splashView)
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+
+        splashView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
