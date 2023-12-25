4d24d8b3afb61a4bb913718bd7bcdbd20f7fa8e2
Bibi-urssu <bibi.urssu@gmail.com>
2023-12-25 21:05:35 +0900

    feat: LoginView, LoginViewController 구현

--- MAKAR/Presentation/Login/LoginViewController.swift
+++ MAKAR/Presentation/Login/LoginViewController.swift
@@ -5,4 +5,57 @@
 //  Created by 박지윤 on 2023/12/25.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: BaseViewController {
+    
+    // MARK: UI Components
+    private let loginView = LoginView()
+
+    // MARK: Environment
+    private let router = BaseRouter()
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        router.viewController = self
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+
+        view.addSubview(loginView)
+
+        loginView.tapAppleLogin = {[weak self] in
+            guard let self else { return }
+            postAppleLogin()
+            router.presentHomeViewController()
+        }
+
+        loginView.tapKakaoLogin = {[weak self] in
+            guard let self else { return }
+            postKakaoLogin()
+            router.presentHomeViewController()
+        }
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+
+        loginView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    // MARK: Networking
+    private func postAppleLogin() {
+        print("postAppleLogin called")
+    }
+
+    private func postKakaoLogin() {
+        print("postKakaoLogin called")
+    }
+}
