b946a516f4a9fb7969117d306cf7c7fadf2c2518
daeun084 <030804jk@naver.com>
2023-12-26 21:33:22 +0900

    feat: HomeViewController 구현

--- MAKAR/Presentation/Home/HomeViewController.swift
+++ MAKAR/Presentation/Home/HomeViewController.swift
@@ -8,8 +8,49 @@
 import UIKit
 
 class HomeViewController: BaseViewController {
+    
+    // MARK: UI Components
+    private let homeView = HomeView()
+    
+    // MARK: Environment
+
+
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .background
     }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+
+        view.addSubview(homeView)
+        
+        homeView.tapSetRouteButton = {[weak self] in
+            guard let self else { return }
+    
+            let SearchRouteVC = SearchRouteViewController()
+            SearchRouteVC.modalPresentationStyle = .fullScreen
+            self.present(SearchRouteVC, animated: true, completion: nil)
+            
+            postSetRouteButtonClicked()
+        }
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+
+        homeView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    // MARK: Networking
+    private func postSetRouteButtonClicked() {
+        print("setRouteButton clicked")
+    }
+    
 }
