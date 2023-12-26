c952e7afb2c4dbb49c32253c96e54d894e7f10ab
daeun084 <030804jk@naver.com>
2023-12-26 21:33:22 +0900

    feat: HomeViewController 구현

--- MAKAR/Presentation/Home/HomeView.swift
+++ MAKAR/Presentation/Home/HomeView.swift
@@ -9,4 +9,47 @@ import UIKit
 
 class HomeView: BaseView {
     
+    // MARK: Constants
+    private enum Metric {
+        static let buttonHeight = 45
+        static let buttonRadius = 16
+    }
+    
+    // MARK: UI Components
+    private let setRouteButton = BaseButton().then {
+        $0.setTitle("경로 설정하기", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .makarBlue
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        $0.layer.cornerRadius = CGFloat(Metric.buttonRadius)
+    }
+    
+    // MARK: Properties
+    var tapSetRouteButton: (() -> Void)?
+    
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+
+        addSubview(setRouteButton)
+        
+        setRouteButton.addTarget(self, action: #selector(handleSetRouteButtonClickEvent), for: .touchUpInside)
+    }
+    
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+        
+        setRouteButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(490)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(Metric.buttonHeight)
+        }
+    }
+    
+    // MARK: Event
+    @objc private func handleSetRouteButtonClickEvent() {
+        tapSetRouteButton?()
+    }
+    
 }
