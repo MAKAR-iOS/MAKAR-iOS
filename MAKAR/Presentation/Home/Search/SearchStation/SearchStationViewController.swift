c128489cc9000a6a78b2f8e09698287db1002bb4
daeun084 <030804jk@naver.com>
2023-12-27 20:04:00 +0900

    feat: SearchStation SearchBar custom

--- MAKAR/Presentation/Home/Search/SearchStation/SearchStationViewController.swift
+++ MAKAR/Presentation/Home/Search/SearchStation/SearchStationViewController.swift
@@ -5,4 +5,33 @@
 //  Created by 박지윤 on 2023/12/25.
 //
 
-import Foundation
+import UIKit
+class SearchStationViewController : BaseViewController {
+    
+    // MARK: UI Components
+    private let searchStationView = SearchStationView()
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .background
+    }
+    
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+        
+        view.addSubview(searchStationView)
+    }
+    
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+        
+        // TODO: Layout 수정
+        searchStationView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
