a4b8d8edc85395fa70f5b3f4c39ed92ab717332d
Bibi-urssu <bibi.urssu@gmail.com>
2024-01-08 22:32:08 +0900

    feat: NotiView, ViewController 구현

---
+++ MAKAR/Presentation/Notification/MakarTableViewCell.swift
@@ -0,0 +1,67 @@
+//
+//  MakarTableViewCell.swift
+//  MAKAR
+//
+//  Created by 박지윤 on 2024/01/08.
+//
+
+import UIKit
+
+final class MakarTableViewCell: UITableViewCell {
+
+    static let identifier = "MakarTableViewCell"
+
+    private(set) var makarNameLabel = UILabel().then {
+        $0.text = "막차 알림1"
+        $0.textColor = .darkGray
+        $0.font = .systemFont(ofSize: 16, weight: .medium)
+    }
+
+    private(set) var makarTimeLabel = UILabel().then {
+        $0.text = "10분 전"
+        $0.textColor = .makarBlue
+        $0.font = .systemFont(ofSize: 16, weight: .bold)
+    }
+
+    private(set) var deleteButton = BaseButton().then {
+        $0.setImage(MakarButton.deleteButton, for: .normal)
+    }
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureSubviews()
+        makeConstraints()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureSubviews() {
+        contentView.addSubview(makarNameLabel)
+        contentView.addSubview(makarTimeLabel)
+        contentView.addSubview(deleteButton)
+    }
+
+    private func makeConstraints() {
+        makarNameLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+        }
+
+        makarTimeLabel.snp.makeConstraints {
+            $0.leading.equalTo(makarNameLabel.snp.trailing).offset(14)
+            $0.centerY.equalToSuperview()
+        }
+
+        deleteButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(16)
+            $0.centerY.equalToSuperview()
+            $0.height.width.equalTo(23)
+        }
+    }
+}
