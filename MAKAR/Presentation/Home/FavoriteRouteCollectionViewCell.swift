7811595e5cb8ccdc5ef1d0e5ffa51f24ae84f5a5
daeun084 <030804jk@naver.com>
2024-01-09 19:40:03 +0900

    add: 즐겨찾는 경로 collectionView 함수 설정

---
+++ MAKAR/Presentation/Home/FavoriteRouteCollectionViewCell.swift
@@ -0,0 +1,11 @@
+//
+//  FavoriteRouteCollectionViewCell.swift
+//  MAKAR
+//
+//  Created by 김다은 on 1/9/24.
+//
+
+import UIKit
+class FavoriteRouteCollectionViewCell : UICollectionViewCell {
+    
+}
