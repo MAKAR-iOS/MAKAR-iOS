2a41105a886907ebce146473988bd65ea6237900
daeun084 <030804jk@naver.com>
2024-01-11 20:51:17 +0900

    feat: 푸시 알림 설정 테이블뷰 UI 구현

--- MAKAR/Presentation/MyPage/PushAlarm/PushAlarmViewController.swift
+++ MAKAR/Presentation/MyPage/PushAlarm/PushAlarmViewController.swift
@@ -7,9 +7,88 @@
 
 import UIKit
 class PushAlarmViewController : BaseViewController {
+    
+    let pushAlarmTableView = UITableView(frame: .zero, style: .grouped)
+    let pushAlarmTableViewCellList : [[String]] = [["전체 알림"], ["막차 알림", "하차 알림"]]
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .background
+        setTableView()
+    }
+    
     // MARK: NavigationBar
     override func setNavigationBar(){
         super.setNavigationBar()
         navigationItem.title = "푸시 알림"
+    }
+}
+
+    // MARK: TableView
+extension PushAlarmViewController : UITableViewDelegate, UITableViewDataSource {
+    //cell
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pushAlarmTableViewCellList[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pushAlarmTableViewCell") ?? UITableViewCell(style: .default, reuseIdentifier: "pushAlarmTableViewCell")
+        cell.textLabel?.text = pushAlarmTableViewCellList[indexPath.section][indexPath.row]
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 16)
+        cell.textLabel?.textColor = .darkgray
+        cell.backgroundColor = .background
+        cell.accessoryView = UISwitch()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if(indexPath.section == 0){
+           //전체 알림
+        } else {
+            if(indexPath.row == 0){
+                //막차 알림
+            } else {
+                //하차 알림
+            }
+        }
+    }
+    
+    //header
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    
+    //footer
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView(frame: .zero)
+        footerView.backgroundColor = .divider
+        return footerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if(section == 0){
+            return 3
+        } else {
+            return 0
+        }
+    }
+    
+    func setTableView(){
+        view.addSubview(pushAlarmTableView)
+
+        pushAlarmTableView.backgroundColor = .background
+        pushAlarmTableView.separatorStyle = .none
+        pushAlarmTableView.sectionHeaderHeight = 0
+        
+        pushAlarmTableView.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        pushAlarmTableView.dataSource = self
+        pushAlarmTableView.delegate = self
     }
 }
