4d60e101454744519f2490614c10eb1ede62230e
daeun084 <030804jk@naver.com>
2024-01-11 19:07:00 +0900

    feat: 마이페이지 프로필 UI 구현

--- MAKAR/Presentation/MyPage/MyPageViewController.swift
+++ MAKAR/Presentation/MyPage/MyPageViewController.swift
@@ -8,11 +8,49 @@
 import UIKit
 
 class MyPageViewController: BaseViewController {
+    
+    // MARK: Constants
+    private enum Metric {
+        static let myPageViewHeight = 70
+    }
 
+    // MARK: UI Components
+    private let myPageView = MyPageView()
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .background
+    }
+    
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+        
+        view.addSubview(myPageView)
+    }
+    
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+        
+        myPageView.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(Metric.myPageViewHeight)
+        }
+    }
+    
+    // MARK: NavigationBar
+    override func setNavigationBar(){
+        super.setNavigationBar()
+        navigationItem.title = "마이페이지"
+    }
+    
 }
 
 extension MyPageViewController {
-    
     private func showLogoutAlert(){
         let logoutAlert = UIAlertController(title: "로그아웃하시겠어요?", message: "", preferredStyle: .alert)
         logoutAlert.addAction( UIAlertAction(title: "로그아웃하기", style: .destructive, handler: {_ in
