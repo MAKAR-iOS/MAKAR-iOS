7bc4acdc712ba1a9deac2374d367b584e62d7ad6
daeun084 <030804jk@naver.com>
2024-01-11 19:07:00 +0900

    feat: 마이페이지 프로필 UI 구현

--- MAKAR/Presentation/MyPage/MyPageView.swift
+++ MAKAR/Presentation/MyPage/MyPageView.swift
@@ -9,4 +9,54 @@ import UIKit
 
 class MyPageView: BaseView {
 
+    // MARK: Constants
+    private enum Metric {
+        static let profileImageSize = 60
+    }
+    
+    // MARK: UI Components
+    private let userProfileImageView = UIImageView().then{
+        $0.image = MakarImage.profile
+    }
+    
+    private let userNameLabel = UILabel().then{
+        $0.text = "김마카"+"님"
+        $0.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+    }
+    
+    private let userLoginLabel = UILabel().then{
+        $0.text = "카카오 로그인 계정"
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.textColor = .lightGray
+    }
+    
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+        
+        addSubview(userProfileImageView)
+        addSubview(userNameLabel)
+        addSubview(userLoginLabel)
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+        
+        userProfileImageView.snp.makeConstraints{
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.equalToSuperview().inset(20)
+            $0.height.width.equalTo(Metric.profileImageSize)
+        }
+        
+        userNameLabel.snp.makeConstraints{
+            $0.top.equalTo(userProfileImageView.snp.top).inset(8)
+            $0.leading.equalTo(userProfileImageView.snp.trailing).inset(-10)
+        }
+        
+        userLoginLabel.snp.makeConstraints{
+            $0.leading.equalTo(userProfileImageView.snp.trailing).inset(-10)
+            $0.bottom.equalTo(userProfileImageView.snp.bottom).inset(8)
+        }
+    }
 }
