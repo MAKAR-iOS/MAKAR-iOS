11d294a837f9b88a2c1f93c2912cbf1e1b6b7aee
daeun084 <030804jk@naver.com>
2024-01-11 20:31:58 +0900

    feat: 마이페이지 테이블뷰 didSelectRowAt 구현

---
+++ MAKAR/Presentation/MyPage/FavoriteRoute/FavoriteRouteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteRouteViewController.swift
+//  MAKAR
+//
+//  Created by 김다은 on 1/11/24.
+//
+
+import UIKit
+class FavoriteRouteViewController : BaseViewController {
+    // MARK: NavigationBar
+    override func setNavigationBar(){
+        super.setNavigationBar()
+        navigationItem.title = "즐겨찾는 경로 설정"
+    }
+}
---
+++ MAKAR/Presentation/MyPage/PushAlarm/PushAlarmView.swift
@@ -0,0 +1,11 @@
+//
+//  PushAlarmView.swift
+//  MAKAR
+//
+//  Created by 김다은 on 1/11/24.
+//
+
+import UIKit
+class PushAlarmView : BaseView {
+    
+}
