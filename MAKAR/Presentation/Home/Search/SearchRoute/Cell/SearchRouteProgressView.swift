cf821aa462f0195843c19dce6a6eb7e7e8489f1a
daeun084 <030804jk@naver.com>
2024-01-21 20:54:23 +0900

    feat: 경로 설정  progressView 요소 구현

---
+++ MAKAR/Presentation/Home/Search/SearchRoute/Cell/SearchRouteProgressView.swift
@@ -0,0 +1,65 @@
+//
+//  SearchRouteProgressView.swift
+//  MAKAR
+//
+//  Created by 김다은 on 1/19/24.
+//
+
+import UIKit
+class SearchRouteProgressView : UIStackView {
+    
+    // MARK: Constants
+    private enum Metric {
+        static let selfHeight = 20
+        static let radius = 10
+    }
+    
+    // MARK: UI Components
+    let lineNumColor = LineNumColor()
+    
+    // MARK: Init
+    init(){
+        super.init(frame: .infinite)
+        
+        configure()
+        makeConstraints()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: Configuration
+    func configure(){
+        backgroundColor = .background
+        layer.cornerRadius = CGFloat(Metric.radius)
+        axis = .horizontal
+        distribution = .fillEqually
+    }
+            
+    // MARK: Layout
+    func makeConstraints() {
+        self.snp.makeConstraints{
+            $0.height.equalTo(Metric.selfHeight)
+        }
+    }
+    
+    func setData(subRouteItemList : [SubRouteItem]){
+        lineNumColor.addLineNum()
+        
+        for subroute in subRouteItemList {
+            let lineNum = subroute.subRoute.lineNum
+            let totalTime = subroute.subRoute.sectionTime
+            let transferTime = subroute.transferInfo.transferTime
+            
+            let lineProgressView = progressView(color: lineNumColor.lineNumColorMap[lineNum]!, text: "\(totalTime)분")
+            self.addArrangedSubview(lineProgressView)
+            
+            if transferTime != 0{
+                let transferInfoView = progressView(color: .divider, text: "\(transferTime)분")
+                self.addArrangedSubview(transferInfoView)
+            }
+        }
+    }
+}
