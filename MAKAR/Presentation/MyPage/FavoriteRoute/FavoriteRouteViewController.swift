cc925528c1c03f0d3c20bccb058a76a4bcf53457
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-26 14:55:42 +0900

    feat: BaseViewController viewTransition 구현

--- MAKAR/Presentation/MyPage/FavoriteRoute/FavoriteRouteViewController.swift
+++ MAKAR/Presentation/MyPage/FavoriteRoute/FavoriteRouteViewController.swift
@@ -16,14 +16,14 @@ class FavoriteRouteViewController : BaseViewController {
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setNavigationBar()
+
         view.backgroundColor = .background
         setTableView()
     }
     
     // MARK: NavigationBar
-    override func setNavigationBar(){
-        super.setNavigationBar()
+    private func setNavigationBar(){
         navigationItem.title = "즐겨찾는 경로 설정"
     }
 }
