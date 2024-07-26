e0679d5c19594e027693de0b426e35c4612aca9c
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-26 12:56:40 +0900

    add: Metric textFieldHeight 추가

--- MAKAR/Extension/Metric.swift
+++ MAKAR/Extension/Metric.swift
@@ -10,6 +10,7 @@ public enum Metric {
     static let logoHeight = 40
     static let buttonHeight = 56
     static let buttonRadius = 8.0
+    static let textFieldHeight = 47
     static let textFieldBorderWidth = 1.5
     static let textFieldRadius = 8.0
 }
