3aadc6babab9d4a4e9aa90824adc0c8ff8aed591
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-26 12:59:10 +0900

    feat: SignInView 및 Router 구현

--- MAKAR/Presentation/Auth/SignIn/SignInViewController.swift
+++ MAKAR/Presentation/Auth/SignIn/SignInViewController.swift
@@ -5,4 +5,46 @@
 //  Created by 박지윤 on 7/23/24.
 //
 
-import Foundation
+import UIKit
+
+class SignInViewController: BaseViewController {
+    
+    // MARK: UI Components
+    private let signInView = SignInView()
+
+    // MARK: Environment
+    private let router = BaseRouter()
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        router.viewController = self
+    }
+
+    // MARK: Configuration
+    override func configureSubviews() {
+        super.configureSubviews()
+
+        view.addSubview(signInView)
+
+        signInView.tapSignInButton = {[weak self] in
+            guard let self else { return }
+            router.presentTabBarViewController()
+        }
+
+        signInView.tapDismissButton = { [weak self] in
+            guard let self else { return }
+            router.dismissViewController()
+        }
+    }
+
+    // MARK: Layout
+    override func makeConstraints() {
+        super.makeConstraints()
+
+        signInView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
