d66fb92c51622c49cd4d2296fb58805e4ece0d2c
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-29 18:48:01 +0900

    feat: Auth API 연결 Setting

--- MAKAR/Networking/Auth/AuthResponse.swift
+++ MAKAR/Networking/Auth/AuthResponse.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct AuthResponse: Codable {
+    let result: String
+    let message: String
+    let data: AuthData
+    let statusCode: Int
+}
+
+struct AuthData: Codable {
+    let accessToken: AuthToken
+    let refreshToken: AuthToken
+}
+
+struct AuthToken: Codable {
+    let token: String
+    let expiredIn: Int
+}
