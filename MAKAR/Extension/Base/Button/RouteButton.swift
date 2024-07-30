0b71effa6fa38aee55faeac35feb3f3bc5c2c2d8
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-30 14:58:55 +0900

    refactor: Base 폴더 구조 정리

--- MAKAR/Extension/Base/Button/RouteButton.swift
+++ MAKAR/Extension/Base/Button/RouteButton.swift
@@ -1,22 +1,21 @@
 //
-//  RouteBaseButton.swift
+//  RouteButton.swift
 //  MAKAR
 //
 //  Created by 김다은 on 12/27/23.
 //
 
 import UIKit
-class BaseRouteButton : BaseButton {
-    
+class RouteButton: BaseButton {
     override init() {
         super.init()
         setComponent()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func setComponent(){
         setTitleColor(.white, for: .normal)
         backgroundColor = .makarBlue
