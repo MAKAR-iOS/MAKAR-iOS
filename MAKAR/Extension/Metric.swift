e73d79768c3717531856f1077c8499c44ee49dcb
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-30 14:59:51 +0900

    refactor: Metric

--- MAKAR/Extension/Metric.swift
+++ MAKAR/Extension/Metric.swift
@@ -8,12 +8,20 @@
 public enum Metric {
     static let logoWidth = 200
     static let logoHeight = 40
-    static let buttonHeight = 56
-    static let buttonRadius = 8.0
+    static let authButtonHeight = 56
+    static let authButtonRadius = 8.0
+    static let makarButtonHeight = 46
+    static let makarButtonRadius = 16.0
     static let textFieldHeight = 47
     static let textFieldBorderWidth = 1.5
     static let textFieldRadius = 8.0
 
-    // MARK: Search
-    static let searchRouteViewHeight = 188
+    
+    // MARK: SearchRoute
+    static let searchRouteViewHeight = 194
+    static let swapButtonHeight = 30
+    static let swapButtonWidth = 30
+    static let textWidth = 40
+    static let searchBarHeight = 38
+    static let searchBarRadius = 18
 }
