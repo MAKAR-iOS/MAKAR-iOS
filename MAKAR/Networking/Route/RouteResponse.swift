47da6c61c72a8dbd630097315ef9ed7c18e86c5c
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-30 22:57:42 +0900

    feat: RouteListResponse 생성

--- MAKAR/Networking/Route/RouteResponse.swift
+++ MAKAR/Networking/Route/RouteResponse.swift
@@ -7,33 +7,6 @@
 
 import Foundation
 
-// MARK: -경로 리스트 조회(검색) API
-struct SearchRouteResponse : Codable {
-    let routeDtoList : [RouteResponse]
-}
-
-// MARK: -경로 설정 API
-struct RouteResponse : Codable {
-    let routeId : Int
-    let sourceStationName : String
-    let sourceLineNum : String
-    let destinationStationName : String
-    let destinationLineNum : String
-    let sourceTime : Date
-    let destinationTime : Date
-    let totalTime : Int
-    let transferCount : Int
-    let subRouteDtoList : [SubRouteResponse]
-}
-
-struct SubRouteResponse : Codable {
-    let fromStationName : String
-    let toStationName : String
-    let lineNum : String
-    let sectionTime : Int
-    let transferTime : Int
-}
-
 // MARK: -설정된 경로 조회 API
 struct RouteDetailResponse : Codable {
     let routeId : Int
