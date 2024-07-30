9484f222abc0c68f64013a38dbe2ff2b8c1b66c6
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-30 22:58:02 +0900

    feat: RouteAPI 구현

--- MAKAR/Networking/Route/RouteAPI.swift
+++ MAKAR/Networking/Route/RouteAPI.swift
@@ -5,4 +5,57 @@
 //  Created by 김다은 on 7/29/24.
 //
 
+import Moya
 import Foundation
+
+enum RouteAPI {
+    case getRouteList(
+        fromStationName: String,
+        fromLineNum: String,
+        toStationName: String,
+        toLineNum: String
+    )
+    case postRoute(routeId: Int)
+    case deleteRoute
+}
+
+extension RouteAPI: TargetType {
+    var path: String {
+        switch self {
+        case .getRouteList, .deleteRoute:
+            return URLConst.route
+        case .postRoute(let routeId):
+            return URLConst.route + "/\(routeId)"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .getRouteList:
+            return .get
+        case .postRoute:
+            return .post
+        case .deleteRoute:
+            return .delete
+        }
+    }
+
+    var task: Moya.Task {
+        switch self {
+        case .getRouteList(let fromStationName,
+                           let fromLineNum,
+                           let toStationName,
+                           let toLineNum):
+            return .requestParameters(
+                parameters: [
+                    "fromStationName": fromStationName,
+                    "fromLineNum": fromLineNum,
+                    "toStationName": toStationName,
+                    "toLineNum": toLineNum
+                ],
+                encoding: URLEncoding.queryString)
+        case .postRoute, .deleteRoute:
+            return .requestPlain
+        }
+    }
+}
--- MAKAR/Networking/Route/RouteService.swift
+++ MAKAR/Networking/Route/RouteService.swift
@@ -6,3 +6,77 @@
 //
 
 import Foundation
+import Moya
+
+final class RouteService {
+
+    private var routeProvider = MoyaProvider<RouteAPI>(plugins: [NetworkLoggerPlugin()])
+
+    private enum ResponseData {
+        case getRouteList
+        case postRoute
+        case deleteRoute
+    }
+
+    public func getRouteList(
+        fromStationName: String,
+        fromLineNum: String,
+        toStationName: String,
+        toLineNum: String,
+        completion: @escaping (NetworkResult<Any>) -> Void ) {
+        routeProvider.request(.getRouteList(
+            fromStationName: fromStationName,
+            fromLineNum: fromLineNum,
+            toStationName: toStationName,
+            toLineNum: toLineNum)
+        ) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+
+                let networkResult = self.judgeStatus(by: statusCode, data, responseData: .getRouteList)
+                completion(networkResult)
+
+            case .failure(let error):
+                let networkResult = self.judgeStatus(by: error.response?.statusCode ?? 500, error.response?.data ?? Data(), responseData: .getRouteList)
+                completion(networkResult)
+                print(error)
+            }
+        }
+    }
+
+    private func judgeStatus(by statusCode: Int, _ data: Data, responseData: ResponseData) -> NetworkResult<Any> {
+
+        let decoder = JSONDecoder()
+
+        switch statusCode {
+        case 200..<300:
+            switch responseData {
+            case .getRouteList, .postRoute, .deleteRoute:
+                return isValidData(data: data, responseData: responseData)
+            }
+        case 400..<500:
+            guard let decodedData = try? decoder.decode(ErrorResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .requestErr(decodedData)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+
+    private func isValidData(data: Data, responseData: ResponseData) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+
+        switch responseData {
+        case .getRouteList, .postRoute, .deleteRoute:
+            guard let decodedData = try? decoder.decode(RouteListResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .success(decodedData)
+        }
+    }
+}
