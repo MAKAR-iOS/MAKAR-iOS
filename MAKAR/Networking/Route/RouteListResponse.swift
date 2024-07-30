58dfc7da149bbdab16cdbb8efa35583457ba82ac
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-30 22:57:42 +0900

    feat: RouteListResponse 생성

---
+++ MAKAR/Networking/Route/RouteListResponse.swift
@@ -0,0 +1,40 @@
+//
+//  RouteListResponse.swift
+//  MAKAR
+//
+//  Created by 박지윤 on 7/30/24.
+//
+
+import Foundation
+
+struct RouteListResponse: Codable {
+    let result: String
+    let message: String
+    let data: RouteListData
+    let statusCode: Int
+}
+
+struct RouteListData: Codable {
+    let routeDtoList: [RouteDTO]
+}
+
+struct RouteDTO: Codable {
+    let routeId: Int
+    let sourceStationName: String
+    let sourceLineNum: String
+    let destinationStationName: String
+    let destinationLineNum: String
+    let sourceTime: Date
+    let destinationTime: Date
+    let totalTime: Int
+    let transferCount: Int
+    let subRouteDtoList: [SubRouteDTO]
+}
+
+struct SubRouteDTO: Codable {
+    let fromStationName: String
+    let toStationName: String
+    let lineNum: String
+    let sectionTime: Int
+    let transferTime: Int
+}
