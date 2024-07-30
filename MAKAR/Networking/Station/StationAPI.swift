7c1a8c0ccfe6a2312a6450e44766f8dcf7c76136
Bibi-urssu <bibi.urssu@gmail.com>
2024-07-30 17:32:57 +0900

    feat: StationAPI 구현

--- MAKAR/Networking/Station/StationAPI.swift
+++ MAKAR/Networking/Station/StationAPI.swift
@@ -5,4 +5,56 @@
 //  Created by 김다은 on 7/29/24.
 //
 
+import Moya
 import Foundation
+
+enum StationAPI {
+    case getStation(query: String)
+    case getFavoriteSchool(query: String)
+    case getFavoriteHome(query: String)
+    case patchFavoriteSchool(stationName: String, lineNum: String)
+    case patchFavoriteHome(stationName: String, lineNum: String)
+}
+
+extension StationAPI: TargetType {
+    var path: String {
+        switch self {
+        case .getStation:
+            return URLConst.getStation
+        case .getFavoriteSchool, .patchFavoriteSchool:
+            return URLConst.favoriteSchool
+        case .getFavoriteHome, .patchFavoriteHome:
+            return URLConst.favoriteHome
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .getStation, .getFavoriteSchool, .getFavoriteHome:
+            return .get
+        case .patchFavoriteSchool, .patchFavoriteHome:
+            return .patch
+        }
+    }
+
+    var task: Moya.Task {
+        switch self {
+        case .getStation(let query):
+            return .requestParameters(parameters: ["query": query], encoding: JSONEncoding.default)
+        case .getFavoriteSchool(let query):
+            return .requestParameters(parameters: ["query": query], encoding: JSONEncoding.default)
+        case .getFavoriteHome(let query):
+            return .requestParameters(parameters: ["query": query], encoding: JSONEncoding.default)
+        case .patchFavoriteSchool(let stationName, let lineNum):
+            return .requestParameters(parameters: [
+                "stationName": stationName,
+                "lineNum": lineNum
+            ], encoding: JSONEncoding.default)
+        case .patchFavoriteHome(let stationName, let lineNum):
+            return .requestParameters(parameters: [
+                "stationName": stationName,
+                "lineNum": lineNum
+            ], encoding: JSONEncoding.default)
+        }
+    }
+}
--- MAKAR/Networking/Station/StationService.swift
+++ MAKAR/Networking/Station/StationService.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+import Moya
+
+final class StationService {
+
+    private var stationProvider = MoyaProvider<StationAPI>(plugins: [NetworkLoggerPlugin()])
+
+    private enum ResponseData {
+        case getStation
+        case getFavoriteSchool
+        case getFavoriteHome
+        case patchFavoriteSchool
+        case patchFavoriteHome
+    }
+
+    public func getStation(query: String, completion: @escaping (NetworkResult<Any>) -> Void ) {
+        stationProvider.request(.getStation(query: query)) { result in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+
+                let networkResult = self.judgeStatus(by: statusCode, data, responseData: .getStation)
+                completion(networkResult)
+
+            case .failure(let error):
+                let networkResult = self.judgeStatus(by: error.response?.statusCode ?? 500, error.response?.data ?? Data(), responseData: .getStation)
+                completion(networkResult)
+                print(error)
+            }
+        }
+    }
+
+    private func judgeStatus(by statusCode: Int, _ data: Data, responseData: ResponseData) -> NetworkResult<Any> {
+
+        let decoder = JSONDecoder()
+
+        switch statusCode {
+        case 200..<300:
+            switch responseData {
+            case .getStation, .getFavoriteSchool, .getFavoriteHome, .patchFavoriteSchool, .patchFavoriteHome:
+                return isValidData(data: data, responseData: responseData)
+            }
+        case 400..<500:
+            guard let decodedData = try? decoder.decode(ErrorResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .requestErr(decodedData)
+        case 500:
+            return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+
+    private func isValidData(data: Data, responseData: ResponseData) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+
+        switch responseData {
+        case .getStation, .getFavoriteSchool, .getFavoriteHome, .patchFavoriteSchool, .patchFavoriteHome:
+            guard let decodedData = try? decoder.decode(StationResponse.self, from: data) else {
+                return .pathErr
+            }
+            return .success(decodedData)
+        }
+    }
+}
