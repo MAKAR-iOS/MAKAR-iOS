6d6e4ef63bf7fa4315f66070ae375d2f16bb79ab
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-01 08:01:59 +0900

    feat: DeleteRouteListResponse 구현

---
+++ MAKAR/Networking/Route/DeleteRouteListResponse.swift
@@ -0,0 +1,13 @@
+//
+//  DeleteRouteListResponse.swift
+//  MAKAR
+//
+//  Created by 박지윤 on 8/1/24.
+//
+
+struct DeleteRouteListResponse: Codable {
+    let result: String
+    let message: String
+    let data: String
+    let statusCode: Int
+}
