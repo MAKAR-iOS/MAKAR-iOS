8bf8c07fd4a93e6e84af27b70b4e34d667a86abd
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-01 06:05:54 +0900

    feat: getRouteList API 구현

---
+++ MAKAR/Networking/Route/FavoriteRouteListResponse.swift
@@ -0,0 +1,15 @@
+//
+//  FavoriteRouteListResponse.swift
+//  MAKAR
+//
+//  Created by 박지윤 on 8/1/24.
+//
+
+import Foundation
+
+struct FavoriteRouteListResponse: Codable {
+    let result: String
+    let message: String
+    let data: [BriefRouteDTO?]
+    let statusCode: Int
+}
