09a31ca5930b63f848859a4f541f26bd5c861b22
daeun084 <030804jk@naver.com>
2024-08-01 01:08:58 +0900

    feat: notiAPI 구현

--- MAKAR/Networking/Noti/NotiResponse.swift
+++ MAKAR/Networking/Noti/NotiResponse.swift
@@ -5,15 +5,21 @@
 //  Created by 김다은 on 7/27/24.
 //
 
+struct NotiResponse: Codable {
+    let result: String
+    let message: String
+    let data: NotiData
+    let statusCode: Int
+}
 // MARK: -알림 추가 API
-struct NotiResponse : Codable {
+struct NotiData : Codable {
     let notiId : Int
     let notiType : String
     let notiMinute : Int
 }
 
 // MARK: -알림 삭제 API / 알림 리스트 조회 API
-struct NotiListResponse : Codable {
-    let makarNotiDtoList : [NotiResponse]
-    let getoffNotiDtoList : [NotiResponse]
+struct NotiListData : Codable {
+    let makarNotiDtoList : [NotiData]
+    let getOffNotiDtoList : [NotiData]
 }
