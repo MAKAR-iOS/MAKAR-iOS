c7dec31838e3b206036233fdce45b8cb1b5d06c4
Bibi-urssu <bibi.urssu@gmail.com>
2024-08-01 15:56:09 +0900

    refactor: FavoriteRouteViewController navigationItem 수정

--- MAKAR/Presentation/MyPage/FavoriteRoute/FavoriteRouteViewController.swift
+++ MAKAR/Presentation/MyPage/FavoriteRoute/FavoriteRouteViewController.swift
@@ -12,26 +12,44 @@ class FavoriteRouteViewController: BaseViewController {
     // MARK: UI Components
     let favoriteRouteTableView = UITableView(frame: .zero, style: .plain)
 
+    private let backButton = BaseButton().then {
+        $0.setImage(MakarButton.dismissButton, for: .normal)
+    }
+
     // TODO: 최근 경로 리스트 조회 API 연결
     var favoriteRouteList: [BriefRouteDTO]? = []
+
+    // MARK: Environment
+    private let router = BaseRouter()
 
     // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        setNavigationBar()
 
         view.backgroundColor = .background
+        router.viewController = self
         setTableView()
     }
 
-    // MARK: NavigationBar
-    private func setNavigationBar() {
+    // MARK: View Transition
+    override func viewTransition() {
+        backButton.tap = { [weak self] in
+            guard let self else { return }
+            router.popViewController()
+        }
+    }
+
+    // MARK: Set Navigation
+    override func setNavigationItem() {
+        super.setNavigationItem()
+
         navigationItem.title = "즐겨찾는 경로 설정"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
     }
 }
 
 // MARK: TableView
-extension FavoriteRouteViewController : UITableViewDelegate, UITableViewDataSource {
+extension FavoriteRouteViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return favoriteRouteList?.count ?? 0
     }
@@ -77,9 +95,9 @@ extension FavoriteRouteViewController : UITableViewDelegate, UITableViewDataSour
         favoriteRouteTableView.backgroundColor = .background
         favoriteRouteTableView.separatorStyle = .none
     
-        favoriteRouteTableView.snp.makeConstraints{
+        favoriteRouteTableView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
-            $0.leading.trailing.bottom.equalToSuperview()
+            $0.horizontalEdges.bottom.equalToSuperview()
         }
 
         favoriteRouteTableView.register(FavoriteRouteTableViewCell.self, forCellReuseIdentifier: "favoriteRouteTableViewCell")
